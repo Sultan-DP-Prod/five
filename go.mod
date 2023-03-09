603b72910b620bbe8d9b1ad69f05fe7a4a01d432
wss-qa <88393634+wss-qa@users.noreply.github.com>
2023-03-09 16:47:48 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/wss-qa/pubmod
+
+go 1.18
+
+require github.com/tdewolff/minify/v2 v2.11.2 // direct
+
+require github.com/tdewolff/parse/v2 v2.5.29 // indirect
---
+++ pubmod.go
@@ -0,0 +1,33 @@
+package pubmod
+
+import (
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/css"
+	"github.com/tdewolff/minify/v2/html"
+	"github.com/tdewolff/minify/v2/js"
+	"github.com/tdewolff/minify/v2/json"
+	"github.com/tdewolff/minify/v2/svg"
+	"github.com/tdewolff/minify/v2/xml"
+)
+
+func Hello() string {
+  return "Hello, You!"
+}
+
+func Goodbye() string {
+  return "Goodbye for now!"
+}
+
+func myminify() {
+	m := minify.New()
+	m.AddFunc("text/css", css.Minify)
+	m.AddFunc("text/html", html.Minify)
+	m.AddFunc("image/svg+xml", svg.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]json$"), json.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]xml$"), xml.Minify)
+
+	if err := m.Minify("text/html", os.Stdout, os.Stdin); err != nil {
+		panic(err)
+	}
+}
